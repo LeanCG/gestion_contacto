33ecef9c2391d5c4284f073f4a7178c9ac8eebb8
LeanCG <leandrocorrea040@gmail.com>
2024-06-26 02:10:55 -0300

    second commit

---
+++ usuarios.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+$conexion = new mysqli("localhost", "root", "L34ndr0.040", "mydb") or die("Problemas de conexion");
+$query = mysqli_query($conexion, "insert into users (nombre,apellido,dni) values ('$_REQUEST[nombre]','$_REQUEST[apellido]','$_REQUEST[dni]')") or die("No se pudo crear el usuario" .mysqli_error($conexion));
+$id_user_inset = mysqli_insert_id($query);
+if ($_REQUEST['telefono']!='' && $_REQUEST['email']!=''){
+  $query =  mysqli_query($conexion, "insert into contacto (telefono,email) values ('$_REQUEST[telefono]','$_REQUEST[email]');") or die("No se pudo crear contacto");
+  $id_contact_id = mysqli_insert_id($query);
+  mysqli_query($conexion, "insert into users_contacto (users_id,contacto_id) values ($id_user_inset,$id_contact_id);") or die("No se pudo crear el usuario");
+}
+mysqli_close($conexion);
+
+
